b31d8fc642b5a44514a577695d92f11be653b5a0
IhorMelnykov <109040072+IhorMelnykov@users.noreply.github.com>
2022-09-20 20:36:01 +0000

    Deploying to gh-pages from @ IhorMelnykov/team-project@237cb7e77eb15c3c0a117fac9b00ff401909f2ae 🚀

--- index.805ccbd4.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".customer-list").slick({infinite:!0,arrows:!1,dots:!0,slidesToShow:1,slidesToScroll:1})}));
-//# sourceMappingURL=index.805ccbd4.js.map
--- index.faf9b9ef.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".customer-list").slick({infinite:!0,arrows:!1,dots:!0,slidesToShow:1,slidesToScroll:1})}));
-//# sourceMappingURL=index.faf9b9ef.js.map

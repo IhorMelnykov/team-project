5e3de0b4cc7beb66ca02d43dac21717b0005aa58
alarvt <alarvt@gmail.com>
2022-09-20 18:45:54 +0200

    header 7

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,17 +1,14 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-      body: document.querySelector("body"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
-    }
-  })();
-  
+  const refs = {
+    openMobilBtn: document.querySelector("[data-menu-open]"),
+    closeMobilBtn: document.querySelector("[data-menu-close]"),
+    mobil: document.querySelector("[data-menu"),
+    body: document.querySelector("body"),
+  };
+  refs.openMobilBtn.addEventListener("click", toggleMobil);
+  refs.closeMobilBtn.addEventListener("click", toggleMobil);
+  function toggleMobil() {
+    refs.mobil.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
